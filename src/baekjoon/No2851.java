1c37dcdeb1da44d6e53647cdb6d644677b42aaa8
dohyeon <dojin8351@gmail.com>
2025-01-27 22:53:22 +0900

    BOJ 2798,2810,3985,2851풀이

---
+++ src/baekjoon/No2851.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class No2851 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int totalScore = 0;
+        int preScore = 0;
+        int curScore = 0;
+        for(int i = 0; i < 10; i++) {
+            int mushroom = sc.nextInt();
+            totalScore += mushroom;
+            if(totalScore >= 100) {
+                curScore = totalScore;
+                preScore = curScore - mushroom;
+                break;
+            } else {
+                curScore = totalScore;
+            }
+        }
+
+        if(Math.abs(preScore - 100) == Math.abs(curScore - 100)) {
+            System.out.println(curScore);
+        } else if(Math.abs(preScore - 100) > (Math.abs(curScore - 100))) {
+            System.out.println(curScore);
+        } else if(Math.abs(preScore - 100) < (Math.abs(curScore - 100))){
+            System.out.println(preScore);
+        } else {
+            System.out.println(curScore);
+        }
+
+    }
+}
