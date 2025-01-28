c7850b8a96fe94ae121f98228a66ccb318bb7e20
dohyeon <dojin8351@gmail.com>
2025-01-29 00:08:29 +0900

    ãBOJ 2839, 10163풀이완 11399 풀이중

---
+++ src/baekjoon/No10163.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class No10163 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] map = new int[1001][1001];
+
+        for(int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int width = sc.nextInt();
+            int height = sc.nextInt();
+
+            for(int j = x; j < x + width; j++) {
+                for(int k = y; k < y + height; k++) {
+                    map[j][k] = i;
+                }
+            }
+        }
+
+        int num = 1;
+        while(num <= n){
+            int cnt = 0;
+            for(int i = 0; i < 1001; i++) {
+                for(int j = 0; j < 1001; j++) {
+                    if(map[i][j] == num) cnt++;
+                }
+            }
+            System.out.println(cnt);
+            num++;
+        }
+
+    }
+}
