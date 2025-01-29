a5b568a5b4d12e4656a8e015ae74bff472d6ce72
dohyeon <dojin8351@gmail.com>
2025-01-30 00:45:34 +0900

    ãBOJ 1158,1139,13300,14696풀이 완료

--- src/baekjoon/No11399.java
+++ src/baekjoon/No11399.java
@@ -1,4 +1,27 @@
 package baekjoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class No11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            int people = sc.nextInt();
+            arr[i] = people;
+        }
+
+        Arrays.sort(arr);
+        int sum = 0;
+        int total = 0;
+        for (int j : arr) {
+            sum += j;
+            total += sum;
+        }
+        System.out.println(total);
+
+    }
 }
---
+++ src/baekjoon/No14696.java
@@ -0,0 +1,53 @@
+package baekjoon;
+
+import java.util.*;
+
+public class No14696 {
+    /*
+        ★ : 4
+        ● : 3
+        ■ : 2
+        ▲ : 1
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            Map<Integer, Integer> aCards = new HashMap<>();
+            Map<Integer, Integer> bCards = new HashMap<>();
+            int a = sc.nextInt();
+            for (int j = 0; j < a; j++) {
+                int card = sc.nextInt();
+                aCards.put(card, aCards.getOrDefault(card, 0) + 1);
+            }
+
+            int b = sc.nextInt();
+            for (int j = 0; j < b; j++) {
+                int card = sc.nextInt();
+                bCards.put(card, bCards.getOrDefault(card, 0) + 1);
+            }
+
+            int[] cardPattern = {4, 3, 2, 1};
+            boolean  resultOfGame= false;
+
+            for(int card : cardPattern) {
+                int countA = aCards.getOrDefault(card, 0);
+                int countB = bCards.getOrDefault(card, 0);
+
+                if(countA > countB) {
+                    System.out.println("A");
+                    resultOfGame = true;
+                    break;
+                } else if(countA < countB) {
+                    System.out.println("B");
+                    resultOfGame = true;
+                    break;
+                }
+            }
+            if(!resultOfGame) {
+                System.out.println("D");
+            }
+        }
+    }
+}
